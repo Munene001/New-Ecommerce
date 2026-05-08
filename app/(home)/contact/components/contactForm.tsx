aa6179d487b63db08afab2e3186a882e961c8fdc
Munene001 <lawrencemunenex@gmail.com>
2026-05-08 17:14:08 +0300

    added site map and about

--- app/(home)/contact/components/contactForm.tsx
+++ app/(home)/contact/components/contactForm.tsx
@@ -69,10 +69,10 @@ export default function ContactForm() {
 
       <div className="max-w-3xl mx-auto">
         <div className="text-center mb-10">
-          <h2 className="text-3xl md:text-4xl font-bold text-white mb-4">
+          <h2 className="text-2xl md:text-4xl font-bold text-white mb-4">
             Send us a Message
           </h2>
-          <p className="text-white text-lg">
+          <p className="text-white text-base">
             Have an inquiry in mind? We&apos;d love to hear from you. Fill out
             the form and we&apos;ll get back to you within 24 hours.
           </p>
