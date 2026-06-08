4218419535ab74a6e537b2ec904c6b2928a97e0f
Munene001 <lawrencemunenex@gmail.com>
2026-06-08 14:21:42 +0300

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -14,5 +14,35 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'monthly',
       priority: 0.8,
     },
+    {
+      url: 'https://paziatech.co.ke/pricing',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://paziatech.co.ke/terms',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://paziatech.co.ke/privacy',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://paziatech.co.ke/auth/login',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.4,
+    },
+    {
+      url: 'https://paziatech.co.ke/auth/shopowner/signup',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.4,
+    },
   ]
 }
